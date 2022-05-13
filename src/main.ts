5281cdd889927262371de0a68ff12b43b4864e0e
limyel <limyel@outlook.com>
2022-05-13 16:56:24 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "@/App.vue"
+import router from "@/router/index"
+import store from "@/store/index"
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import ElementPlus from "element-plus"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus,{locale:zhCn})
+
+app
+    // 挂载路由
+    .use(router)
+    // 挂载 vuex
+    .use(store)
+    .mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 const pathResolve = (dir: string) => resolve(__dirname, dir);
 
 export default defineConfig({
   plugins:[
     vue(),
+    // 按需引入 element plus 组件
     AutoImport({
       resolvers: [ElementPlusResolver()]
     }),
@@ -18,7 +19,23 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": pathResolve("./src"), // 别名
+      // 别名
+      "@": pathResolve("./src"),
+    }
+  },
+  build: {
+    // 主要用于兼容低版本浏览器 可以解决chrome65版本打包发布到服务器上页面空白的问题(主要是65版本不兼容 try catch )
+    target: "es2015",
+    // 兼容低版本浏览器上样式问题
+    cssTarget:"chrome65",
+    // 打包配置路径
+    assetsDir: "./assets",
+    rollupOptions: {
+      // 主要用于配置多页面
+      input: {
+        platForm: resolve(__dirname, "platform.html"),
+        merchant: resolve(__dirname, "merchant.html")
+      }
     }
   },
 })
