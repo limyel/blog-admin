f38471774e0b5ece7d262f157d4ead0e4337f858
limyel <limyel@outlook.com>
2022-05-13 10:23:57 +0800

    vue3+vite+ts

--- src/request/api/admin.js
+++
@@ -1,8 +0,0 @@
-import {post} from "@/request/http";
-
-let baseUrl = '/admin/admins';
-
-export function apiLogin(data) {
-    let url = baseUrl + '/login';
-    return post(url, data);
-}
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from "path";
+const pathResolve = (dir: string) => resolve(__dirname, dir);
+
+export default defineConfig({
+  plugins:[
+    vue()
+  ],
+  resolve: {
+    alias: {
+      "@": pathResolve("./src"), // 别名
+    }
+  }
+})
