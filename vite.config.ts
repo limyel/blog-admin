62d9dd589b74ef0a1b43545ea817592f9ddde897
limyel <limyel@outlook.com>
2022-05-13 10:48:47 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from "path";
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 const pathResolve = (dir: string) => resolve(__dirname, dir);
 
 export default defineConfig({
   plugins:[
-    vue()
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
   ],
   resolve: {
     alias: {
       "@": pathResolve("./src"), // 别名
     }
-  }
+  },
 })
